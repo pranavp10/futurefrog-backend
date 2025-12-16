a398f71485256f49dd22ad87128d487caf0e3439
Pranav <pranavkp.me@outlook.com>
2025-12-16 19:04:44 +0530

    feat: Make server port configurable via environment variable.

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = new Elysia()
   .use(getCoinsRoute)
   .use(cryptoMoversRoutes)
   .get("/", () => "Hello Elysia")
-  .listen(3000);
+  .listen(process.env.PORT || 3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
